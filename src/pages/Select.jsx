2f0b4d4cbf912e2cc369773d3785e91c01fbad00
EtsukoSusui <btsm0714@gmail.com>
2022-02-14 12:01:43 +0900

    aa

--- src/pages/Select.jsx
+++ src/pages/Select.jsx
@@ -244,7 +244,6 @@ function Top() {
             });
             })
         }
-
     }
 
 
@@ -302,7 +301,7 @@ function Top() {
                                 }
                                 return option.label;
                             }}
-                            renderOption={(props, option) => <li {...props}>{option.label}</li>}
+                            renderOption={(props, option) => <li {...props} >{option.label}</li>}
                             renderInput={(params) => <TextField {...params} label="" />}
                         />
                         <button className="search-btn">
@@ -321,10 +320,7 @@ function Top() {
                                         <div style={{backgroundImage:`url(${image.img_url})`}} onClick={()=>handleClickAvatar(image.chara_id)} key={index} className={`${selectedAvatars.map(item=>(item.chara_id)).includes(image.chara_id) ? "active" : ""} character-item`}>
                                             <span></span>
                                         </div>
-                                       
-                                      
                                     ))
-                                   
                                 }
                             </div>
                         }
@@ -338,7 +334,6 @@ function Top() {
                                         </div>
                                     ))
                                 }
-                                
                             </div>
                         }
                         <button onClick={handleOutline} className={`character-add-btn ${active ? "active" : ""}`} disabled={!active}>マぜる</button>
