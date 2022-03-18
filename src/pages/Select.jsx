7aedbe77062f693bdad2c76f8e5f284bc03533a0
EtsukoSusui <btsm0714@gmail.com>
2022-03-18 11:32:38 +0900

    height

--- src/pages/Select.jsx
+++ src/pages/Select.jsx
@@ -47,8 +47,8 @@ function Top() {
     const [width, height] = useWindowSize();
 
     useEffect(async() => {
-        let vh = window.innerHeight;
-        document.getElementById("character_select").style.height = vh + "px";
+        // let vh = window.innerHeight;
+        // document.getElementById("character_select").style.height = vh + "px";
         let register_id =  sessionStorage.register_id || null;
         let background = sessionStorage.background || null;
         let user_list = sessionStorage.user_list || null; 
