2d300f97fdd81267799dc4e45d680c108503cb40
yohei-kobashi <kobashi@watashiha.co.jp>
2022-05-17 07:48:31 +0000

    docker対応

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envPrefix: 'REACT_APP_'
+  server: {
+    host: '0.0.0.0',
+  },
+ envPrefix: 'REACT_APP_'
 })
